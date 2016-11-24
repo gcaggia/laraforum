68598f0189587f1930650db38f8ba9f17f096849
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-11-24 10:00:49 +0000

    init commit and mail config

---
+++ app/Http/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/mail', function () {
+
+    $data = [
+    	'title'   => 'welcome',
+    	'content' => 'A message to say hello !'
+    ];
+
+    Mail::send('email.welcome', $data, function($message){
+    	$message->to(env('MAIL_PERSO'), 'laraforum')->subject('Welcome to Laraforum');
+    });
+
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
