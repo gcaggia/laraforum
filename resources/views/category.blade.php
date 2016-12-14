6c5cd98916c1f528a71073ecf92804da96ffef47
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-12-15 01:58:20 +1000

    category view update

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -9,8 +9,39 @@
 <!-- Main title in the page -->
 @section('main-title' , 'LaraForum - ' . $category->title)
 
-<!-- content of the page -->
 @section('main-content')
+    <p>{{ $category->description }}</p>
+    <hr>
+    <table class="table table-topics">
+    	<thead>
+    		<tr>
+    			<th>Title</th>
+    			<th>Posts</th>
+    			<th>Last Message</th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		@foreach ($category->topics as $topic)
+		    	<tr>
+		    		<td>
+		    			<h3>
+		    				<a href="{{ $category->title . '/' . $topic->id }}" 
+					    	   class="">
+					    		{{ $topic->title }}
+					    	</a>
+		    			</h3>
+		    			<p>Created by <a href="#">User</a> - 02-13-2015 03:25 PM</p>
+		    		</td>
+		    		<td>10 Posts</td>
+		    		<td><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></td>
+		    	</tr>
+	    	@endforeach
+    	</tbody>
+    </table>
+@endsection
+
+<!-- content of the page -->
+<!-- @section('main-content')
     <p>{{ $category->description }}</p>
     <hr>
     <div class="list-group">
@@ -21,4 +52,4 @@
 	    	</a>
 	    @endforeach
     </div>
-@endsection
+@endsection -->
