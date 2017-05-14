079a849c5237878781c7d0c2f8d194e753a4c58a
Guillaume Caggia <guillaume.caggia@gmail.com>
2017-05-15 05:39:34 +1000

    change title app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'LaraForum') }}
+                LaraForum
             </a>
         </div>
 
